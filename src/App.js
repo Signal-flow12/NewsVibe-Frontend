e53602b8c8cdb91ea5594c960e32e4bd7b18c08b
Signal-flow12 <tcravenny@gmail.com>
2023-07-17 20:55:09 -0400

    Created edit route for post

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Post from './pages/Post';
 import PostDelete from './pages/PostDelete';
 import PostShow from './pages/PostShow';
 import Header from './components/Header';
+import PostEdit from './pages/PostEdit';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Post />} />
           <Route path="/:id" element={<PostShow />} />
           <Route path='/:id/delete' element={<PostDelete />} />
+          <Route path='/:id/edit' element={<PostEdit />} />
         
         </Routes>
       </header>
---
+++ src/pages/PostEdit.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams} from "react-router-dom"
+
+const PostEdit = () => {
+
+    const { id } = useParams();
+    const [post, setPost] = useState(null);
+    const navigate = useNavigate();
+    const URL = `http://localhost:4000/${id}`
+  
+
+    useEffect(() => {
+        async function getPost() {
+            try {
+                const request = await fetch(URL);
+                const response = await request.json();
+                setPost(response);
+            } catch(err) {
+                console.log(err);
+            }
+        }
+        getPost();
+    }, [id]);
+
+    function handleChange(e) {
+        setPost((currentState) => ({
+            ...currentState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    async function handleSumbit(e) {
+        try{
+            e.preventDefault();
+            await fetch(URL, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(post)
+            });
+            return navigate(`/${id}`);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    function loaded() {
+        return(
+            <>
+                <h2>Edit</h2>
+                <form onSubmit={handleSumbit}>
+                    Post: <input type="text" value={post.text} name="title" onChange={handleChange} />
+                    User: <input type="text" value={post.image} name="author" onChange={handleChange} />
+                    <button>Submit</button>
+                </form>
+            </>
+        )
+    }
+
+    return (
+        <>
+            {post ? loaded() : <h2>Loading...</h2>}
+        </>
+    )
+}
+
+export default PostEdit;
