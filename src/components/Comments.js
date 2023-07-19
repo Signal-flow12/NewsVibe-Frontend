b415b6f6832f8983a4a5b032403c995052fe1cf9
Signal-flow12 <tcravenny@gmail.com>
2023-07-19 17:51:04 -0400

    Fixed lint rules

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -25,6 +25,7 @@ const Comments = ({ postId }) => {
 
   useEffect(() => {
     getComments();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleChange = (e) => {
